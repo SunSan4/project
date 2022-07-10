804ad526ef6379132846454f0ab399ddf7f953af
sansan4 <ss@sunsan4.net>
2022-07-10 18:05:41 +0300

    up

--- client/pages/index.js
+++ client/pages/index.js
@@ -158,9 +158,9 @@ const handleSublit = async (event) => {
   
   <Layout>
             <Button.Group basic vertical>
-      <Button>BUSD</Button>
-      <Button>USDT</Button>
-      <Button>USDC</Button>
+      <Button onClick={()=>settokenAddress("0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56")} >BUSD</Button> 
+      <Button onClick={()=>settokenAddress("0x55d398326f99059fF775485246999027B3197955")} >USDT</Button> 
+      <Button onClick={()=>settokenAddress("0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d")} >USDC</Button> 
     </Button.Group>
 
     {/* <Button.Group>
